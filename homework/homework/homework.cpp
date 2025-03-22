4de81b57731e80219e5f522cb9c3bd3bc1ae6fcb
aaaaaa5647 <nominaalexeeva@gmail.com>
2025-03-22 18:49:10 +0800

    дз

---
+++ homework/homework/homework.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+class Printer
+{
+    queue<string> q;
+public:
+    int AddToPrint(string doc)
+    {
+        q.push(doc);
+        return q.size() - 1;
+    }
+    int Print()
+    {
+        cout << q.front();
+        q.pop();
+        return q.size();
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    Printer printer;
+    cout << "Добавлен документ под номером " << printer.AddToPrint("Первый документ\nПривет мир\n") << endl;
+    cout << "Добавлен документ под номером " << printer.AddToPrint("Второй документ\nПока мир\n") << endl;
+    cout << "Добавлен документ под номером " << printer.AddToPrint("Третий документ\nСнова привет мир\n") << endl;
+
+    while (printer.Print());
+    return 0;
+}
